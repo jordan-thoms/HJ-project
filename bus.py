e83b225bc08f9021d9c9428669045a11332f5440
Hengjie <hengjie@gmail.com>
2012-01-15 22:44:54 -0800

    Curated data into dict() from the Maxx website.

--- bus.py
+++ bus.py
@@ -15,18 +15,42 @@ import sys
 import os
 from BeautifulSoup import BeautifulSoup	# For processing HTML
 
+import urllib
+import urllib2
+
 import web
 
 urls = (
-    '/(.*)', 'hello'
+    '/(.*)/(.*)', 'bus',
 )
 app = web.application(urls, globals())
 
-class hello:        
-    def GET(self, name):
-        if not name: 
-            name = 'World'
-        return 'Hello, ' + name + '!'
+class bus:        
+    def GET(self, origin, dest):
+        # Get the bus stop number and relevant bus numbers to destination
+
+		# Assuming we know it, for dev purposes, we'll hardcode this
+		# 7148 - 36 Symonds St - S3
+		bus_stop_number = 7148
+		
+		# ?stop=7148
+		headers = { 'User-Agent' : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.75 Safari/535.7" }
+		
+		request = urllib2.Request(url=("http://m.maxx.co.nz/mobile-departure-board.aspx?stop=" + str(bus_stop_number)), headers=headers)
+		response = urllib2.urlopen(request)
+		
+		soup = BeautifulSoup(response)
+		incoming_buses = soup.findAll('tr', attrs={'data-theme' : 'a'})
+		
+		bus_stop_parsed = []
+		for row in incoming_buses:
+			incoming_bus_number = row.contents[1].contents
+			incoming_bus_dest = row.contents[3].contents # bound dest
+			incoming_bus_due = row.contents[5].contents # in minutes
+			
+			bus_stop_parsed.append({'bus_number': incoming_bus_number, 'bus_dest': incoming_bus_dest, 'bus_due': incoming_bus_due})
+		
+		return bus_stop_parsed
 
 if __name__ == "__main__":
     app.run()
